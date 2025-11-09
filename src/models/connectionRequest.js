339b054fafb18ae26001edda66b5ad9dee99c433
UserShri98 <sjc3098@gmail.com>
2025-11-09 17:21:00 +0530

    Created a schema for sending a connection request to the user and also designed an API for the same

---
+++ src/models/connectionRequest.js
@@ -0,0 +1,39 @@
+
+const mongoose=require('mongoose');
+
+const connectionRequestSchema=new mongoose.Schema({
+    fromUserId:{
+      type:mongoose.Schema.Types.ObjectId,
+      required:true,
+    },
+     toUserId:{
+      type:mongoose.Schema.Types.ObjectId,
+      required:true,
+    },
+    status:{
+        type:String,
+        required:true,
+        enum:{
+            values:["ignored","accepted","rejected","interested"],
+            message:'{VALUE} is not accepted'
+        }
+    }
+},
+{timestamps:true}
+)
+
+connectionRequestSchema.index({toUserId:1,fromUserId:1})
+
+connectionRequestSchema.pre("save",function(next){
+   const connectionRequest=this;
+
+   if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+     throw new Error("Cannot have same ids for both sender and receiver")
+
+   }
+   next();
+})
+
+const connectionRequestModel=mongoose.model("ConnectionRequest",connectionRequestSchema);
+
+module.exports=connectionRequestModel;
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,13 +1,52 @@
 const express=require('express');
 const app=express();
 const requestRouter=express.Router();
-const userauth=require('../middlewares/auth')
+const userauth=require('../middlewares/auth');
+const connection= require('../models/connectionRequest');
+const User=require('../models/user')
 
 
-requestRouter.post('/sendconnectionrequest',userauth,(req,res)=>{
-  const user=req.user;
+requestRouter.post('/send/request/:status/:toUserId',userauth,async(req,res)=>{
+  try{
 
-  res.send(user.firstName + " sent a connection request")
+    const fromUserId=req.user._id;
+    const toUserId=req.params.toUserId;
+    const status=req.params.status;
+
+    const isUserInDb=await User.findById(toUserId);
+    if(!isUserInDb){
+      return res.status(400).json({message:"User not found"})
+    }
+
+    const existingConnection=await connection.findOne({
+      $or:[
+        {toUserId,fromUserId},
+        {toUserId:fromUserId,fromUserId:toUserId}
+      ]
+    })
+    if(existingConnection){
+      return res.status(400).json({message:"Connection request already exists"})
+    }
+
+    const allowedStatus=["interested","ignored"];
+
+    if(!allowedStatus.includes(status)){
+      throw new Error("This status is not allowed",status)
+    }
+
+    const connectionRequest= await new connection({
+      toUserId,
+      fromUserId,
+      status
+    })
+
+    const data=await connectionRequest.save();
+
+    res.json({message:status==="interested"? `${req.user.firstName} has sent a request to ${isUserInDb.firstName}`:`${isUserInDb.firstName} has ignored the request `},data)
+
+  }catch(err){
+    res.status(400).send("Error: "+err.message);
+  }
   
 })
 
