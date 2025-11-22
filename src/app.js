e1f4ecb0875e47de03db23f36fae1c7a8854bdb9
UserShri98 <sjc3098@gmail.com>
2025-11-22 09:34:06 +0530

    Revert "changes in app.js"
    This reverts commit 8a617bd56906f9c5ca496d8a304d913e2ccaeac1.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const express=require('express');
+const adminAuth=require('./middlewares/auth')
 const app=express();
 const connectDB=require('./config/db')
+const User=require('./models/user')
+const jwt=require("jsonwebtoken");
 const cookieParser = require('cookie-parser');
 const cors=require('cors')
 
-// CORS configuration - allow both development and production origins
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://16.171.30.18'],
+    origin:'http://localhost:5173',
     credentials:true,
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
+    allowedHeaders: ['Content-Type']
 }))
 app.use(express.json())
 app.use(cookieParser())
@@ -19,11 +21,10 @@ const profileRouter=require('./routes/profile')
 const requestRouter=require('./routes/request');
 const userRouter = require('./routes/user');
 
-// Mount all routes under /api prefix
-app.use('/api',authRouter);
-app.use('/api',profileRouter);
-app.use('/api',requestRouter)
-app.use('/api',userRouter)
+app.use('/',authRouter);
+app.use('/',profileRouter);
+app.use('/',requestRouter)
+app.use('/',userRouter)
 
 // app.post('/users', async (req, res) => {
 
