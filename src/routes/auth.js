9fd7c41474600d3a3345bbcb90cb6a5efdb80186
UserShri98 <sjc3098@gmail.com>
2026-01-07 12:18:14 +0530

    changed the cookie configuration in signup logic

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -5,30 +5,41 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 
-authRouter.post('/signup', async (req, res) => {
+authRouter.post("/signup", async (req, res) => {
   try {
-
-    try {
-      validation(req);
-    } catch (err) {
-      return res.status(400).json({ error: err.message });
-    }
+    validation(req);
 
     const { firstName, lastName, email, age, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = new User({ firstName, lastName, email, age, password: hashedPassword });
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      age,
+      password: hashedPassword
+    });
+
     const savedUser = await user.save();
-
     const token = await savedUser.getJWT();
-    res.cookie("token", token, { expires: new Date(Date.now() + 8 * 360000) });
 
-    return res.json({ message: "User added successfully", data: savedUser });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: 8 * 60 * 60 * 1000 // 8 hours
+    });
+
+    return res.json({
+      message: "User added successfully",
+      data: savedUser
+    });
 
   } catch (err) {
     return res.status(400).json({ error: err.message });
   }
 });
+
 
 
 authRouter.post('/login', async (req, res) => {
